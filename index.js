93b545399c29b9e16a30251c211cab613428e6e5
Lance Hebert <LSUHEBERT@gmail.com>
2021-10-21 20:14:04 -0700

    Dom Modifying Elements lab completed

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+// Write your code herem
+
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent = "Lance is the champion";
