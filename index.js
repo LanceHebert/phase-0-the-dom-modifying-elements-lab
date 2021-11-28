43c104a01ea84cffae0b08286fd2c5d26e7635f3
Lance Hebert <LSUHEBERT@gmail.com>
2021-11-27 21:29:11 -0800

    commit tree b40fa3aa4dd53f23cf006565e5b3737ae734ea2a

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 // Write your code herem
 
 main.remove();
+const ul = document.createElement("ul");
 
 const newHeader = document.createElement("h1");
+const body = document.querySelector("body");
+body.append(newHeader,ul);
+
+for(let i = 0; i <3; i++)
+{
+    const newLi = document.createElement("li")
+    newLi.textContent = i+1
+    ul.append(newLi);
+       
+}
+
+const child2 = ul.children[1];
+const child3 = ul.children[2];
+child2.style.color = "blue";
+child3.style.fontSize = "64px"; 
+
+console.log(child2);
+// console.log(newHeader);
 
 newHeader.setAttribute("id", "victory");
 
 newHeader.textContent = "Lance is the champion";
+
+
